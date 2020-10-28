e2d5f5039ce09b1cfe814f7d21890b662c3c85b4
Yauheni Yafimenka <yafimenka.jk@gmail.com>
2020-10-28 14:15:25 +0300

    style: fix styles of CV

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import('./scss/styles.scss').then(async () => {
   const content = tbody.content.cloneNode(true);
   document.body.appendChild(content);
   await import('@fortawesome/fontawesome-free/js/fontawesome');
-  await import('@fortawesome/fontawesome-free/js/solid');
+  import('@fortawesome/fontawesome-free/js/solid');
+  import('@fortawesome/fontawesome-free/js/brands');
 });
 
